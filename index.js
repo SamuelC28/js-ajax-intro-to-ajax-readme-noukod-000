7abb7c2d0b5572ab479618b7cd2566b829e456a0
Samuel Cornet <samuel@noukod.com>
2019-02-18 17:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ function showRepositories() {
 }
 
 function getRepositories() {
-  console.log(this.responseText);
-let repoList = '<ul>';
-for (var i = 0; i < this.responseText.length; i++) {
-  repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-}
-repoList += '</ul>';
-document.getElementById('repositories').innerHTML = repoList;
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+  .map(r => '<li>' + r.name + '</li>')
+  .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
   // const req = new XMLHttpRequest();
   // req.addEventListener('load', showRepositories);
   // req.open('GET', 'https://api.github.com/users/octocat/repos');
