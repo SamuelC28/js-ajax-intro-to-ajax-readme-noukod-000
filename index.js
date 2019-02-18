55bf3676edbe7594e37fb638b6ecabd1879b0b46
Samuel Cornet <samuel@noukod.com>
2019-02-18 18:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function showRepositories() {
 }
 
 function getRepositories() {
-  function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', showRepositories)
+    req.open('GET', 'https://api.github.com/users/octocat/repos');
+    req.send();
 }
 
 
